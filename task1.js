d6a7fad85ea34cc5361e76bb5997977cdb8f6a1c
DanielleSaint <daniellesaint09@gmail.com>
2025-03-31 11:10:36 -0500

    Tarea 1 realizada

--- task1.js
+++ task1.js
@@ -1,6 +1,6 @@
-//*1. Maria is calculating the cost of monthly payments. For every transaction there is a $3 fee and a 1% (0.01) interest fee. 
-//  * Given an input transaction amount, export a function that returns the value of what she should be paying.
-//  * This function should be able to take a number as input, and return a number as output.
+//1. Maria is calculating the cost of monthly payments. For every transaction there is a $3 fee and a 1% (0.01) interest fee. 
+//   Given an input transaction amount, export a function that returns the value of what she should be paying.
+//   This function should be able to take a number as input, and return a number as output.
 
 //const prompt = require("prompt-sync")();
 import promptSync from 'prompt-sync';
