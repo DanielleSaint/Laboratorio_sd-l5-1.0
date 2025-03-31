1ff3cd9af18ca2ec445b9284f787ca3cc8491d4c
DanielleSaint <daniellesaint09@gmail.com>
2025-03-31 11:54:51 -0500

    tarea 5 realizada

--- task5.js
+++ task5.js
@@ -1,3 +1,12 @@
-export function rubricPassFail() {
+// 5. A teacher wants to create a rubric for grading students based on a score from 0 to 11.
+//A student passes if they have a score greater than or equal to 5. Export a function that returns "Pass" or "Fail".
 
+export function rubricPassFail(score) {
+    if (score >= 5){
+        console.log("Pass");
+    }else {
+        console.log("Fail");
+    }
 }
+
+rubricPassFail(5);
